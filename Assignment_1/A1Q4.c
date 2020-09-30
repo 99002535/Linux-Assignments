dd171aabc9ab79c2ed042b9b0a571a697d5273c4
99002535 <71541632+99002535@users.noreply.github.com>
2020-09-30 18:34:53 +0530

    Update A1Q4.c

--- Assignment_1/A1Q4.c
+++ Assignment_1/A1Q4.c
@@ -1,14 +1,48 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int main(void) {
-  char *programName ;
-  printf("Enter your command:");
-	scanf("%s",programName);
-  char *arg1 = "-lh";
-  char *arg2 = "/home";
- 
-  execlp(programName, programName, arg1, arg2, NULL);
- 
-  return 0;
+#include  <stdio.h>
+#include  <sys/types.h>
+void  parse(char *line, char **argv)
+{
+     while (*line != '\0') {      
+          while (*line == ' ' || *line == '\t' || *line == '\n')
+               *line++ = '\0';    
+          *argv++ = line;        
+          while (*line != '\0' && *line != ' ' &&
+                 *line != '\t' && *line != '\n')
+               line++;            
+     }
+     *argv = '\0';                
+}  
+void  execute(char **argv)
+{
+     pid_t  pid;
+     int    status;
+    
+     if ((pid = fork()) < 0) {    
+          printf("*** ERROR: forking child process failed\n");
+          exit(1);
+     }
+     else if (pid == 0) {         
+          if (execvp(*argv, argv) < 0) {   
+               printf("*** ERROR: exec failed\n");
+               exit(1);
+          }
+     }
+     else {                                
+          while (wait(&status) != pid);
+     }
+}
+void  main(void)
+{
+     char  line[1024];            
+     char  *argv[64];             
+    
+     while (1) {                  
+          printf("Shell -> ");   
+          gets(line);             
+          printf("\n");
+          parse(line,argv);     
+          if (strcmp(argv[0], "exit") == 0)
+               exit(0);         
+          execute(argv);          
+     }
 }
